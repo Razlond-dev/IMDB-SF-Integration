16ac100a3e62e328b5af89c0bb2cb1f5ae5b0bd7
Nazar Pidhirnyi <n.pidhirnyi@viseven.com>
2022-11-02 20:01:23 +0200

    add pagination and filters to favorites page

--- force-app/main/default/lwc/imdbAdvancedSearch/imdbAdvancedSearch.js
+++ force-app/main/default/lwc/imdbAdvancedSearch/imdbAdvancedSearch.js
@@ -1,7 +1,8 @@
 /* eslint-disable guard-for-in */
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
 export default class ImdbAdvancedSearch extends LightningElement {
+    @api isReleaseDateFilteringAllowed = false;
     genresValue = [];
     genresOptions = [
         { label: 'Action', value: 'Action' },
@@ -41,22 +42,37 @@ export default class ImdbAdvancedSearch extends LightningElement {
     ];
 
     sortByValue = '';
-    sortByOptions = [
-        { label: 'A-Z Ascending', value: 'alpha,asc' },
-        { label: 'A-Z Descending', value: 'alpha,desc' },
-        { label: 'User Rating Ascending', value: 'user_rating,asc' },
-        { label: 'User Rating Descending', value: 'user_rating,desc' },
-        { label: 'Num Votes Ascending', value: 'num_votes,asc' },
-        { label: 'Num Votes Descending', value: 'num_votes,desc' },
-        { label: 'Runtime Ascending', value: 'runtime,asc' },
-        { label: 'Runtime Descending', value: 'runtime,desc' },
-        { label: 'Release Date Ascending', value: 'release_date,asc' },
-        { label: 'Release Date Descending', value: 'release_date,desc' }
-    ];
+    get sortByOptions() {
+        let options = [
+            { label: 'A-Z Ascending', value: 'alpha,asc' },
+            { label: 'A-Z Descending', value: 'alpha,desc' },
+            { label: 'User Rating Ascending', value: 'user_rating,asc' },
+            { label: 'User Rating Descending', value: 'user_rating,desc' },
+            { label: 'Num Votes Ascending', value: 'num_votes,asc' },
+            { label: 'Num Votes Descending', value: 'num_votes,desc' },
+            { label: 'Runtime Ascending', value: 'runtime,asc' },
+            { label: 'Runtime Descending', value: 'runtime,desc' }
+        ];
+        let releaseDateOptions = [
+            { label: 'Release Date Ascending', value: 'release_date,asc' },
+            { label: 'Release Date Descending', value: 'release_date,desc' }
+        ];
+
+        if (this.isReleaseDateFilteringAllowed) {
+            options.push(...releaseDateOptions);
+        }
+
+        return options;
+    }
 
     submitHandler(event) {
         event.preventDefault();
-        let numberInputNames = ['fromNumberOfVotes', 'toNumberOfVotes', 'fromRuntime', 'toRuntime'];
+        let numberInputNames = [
+            'fromNumberOfVotes',
+            'toNumberOfVotes',
+            'fromRuntime',
+            'toRuntime'
+        ];
         const formData = new FormData(event.target);
         const formProps = Object.fromEntries(formData);
         formProps.genres = this.genresValue;
@@ -66,17 +82,24 @@ export default class ImdbAdvancedSearch extends LightningElement {
             if (formProps[property] === '') {
                 formProps[property] = null;
             }
-            if (numberInputNames.includes(property) && formProps[property] != null) {
+            if (
+                numberInputNames.includes(property) &&
+                formProps[property] != null
+            ) {
                 formProps[property] = formProps[property].replace(/\s/g, '');
-            } 
+            }
             if (property === 'fromUserRating' && formProps[property] !== null) {
-                formProps[property] = formProps[property].toString().replace(',', '.');
+                formProps[property] = formProps[property]
+                    .toString()
+                    .replace(',', '.');
             }
             if (property === 'toUserRating' && formProps[property] !== null) {
-                formProps[property] = formProps[property].toString().replace(',', '.');
+                formProps[property] = formProps[property]
+                    .toString()
+                    .replace(',', '.');
             }
         }
-        console.log('FORM PROPS - '+JSON.stringify(formProps));
+        console.log('FORM PROPS - ' + JSON.stringify(formProps));
         this.dispatchEvent(
             new CustomEvent('search', {
                 detail: { value: formProps }
@@ -99,5 +122,4 @@ export default class ImdbAdvancedSearch extends LightningElement {
     toggleFilters() {
         this.template.querySelector('.slide-up').classList.toggle('slide-down');
     }
-
 }
--- force-app/main/default/lwc/imdbFavorites/imdbFavorites.js
+++ force-app/main/default/lwc/imdbFavorites/imdbFavorites.js
@@ -1,12 +1,49 @@
 import { LightningElement } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import queryAddedMovies from '@salesforce/apex/IMDB_FavoritesCtrl.queryAddedMovies';
+// labels
+import Error_text from '@salesforce/label/c.Error_text';
+import Records_updated_error from '@salesforce/label/c.Records_updated_error';
 
 export default class ImdbFavorites extends LightningElement {
     responseFromAPI;
     arrayOfMovies = [];
+    arrayOfMoviesBefore;
 
     async connectedCallback() {
         let response = await queryAddedMovies();
-        this.arrayOfMovies = JSON.parse(response);
+        this.arrayOfMoviesBefore = JSON.parse(response);
+    }
+
+    async queryAddedMovies(filterParameters) {
+        try {
+            console.log('value2 - '+JSON.stringify(filterParameters));
+            let responseFromAPI = await queryAddedMovies({
+                jsonFilterParameters: JSON.stringify(filterParameters)
+            });
+            console.log('MOVIES');
+            let resultArray = JSON.parse(responseFromAPI);
+            console.log(JSON.stringify(resultArray));
+            this.arrayOfMoviesBefore = [...resultArray];
+        } catch (error) {
+            console.log(error);
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: Error_text,
+                    message: Records_updated_error,
+                    variant: 'error'
+                })
+            );
+        }
+    }
+
+    searchHandler(event) {
+        console.log('value - '+JSON.stringify(event.detail.value));
+        this.queryAddedMovies(event.detail.value);
+    }
+
+    paginateHandler(event){
+        this.arrayOfMovies = [...event.detail.records];
+        console.log(JSON.stringify(event.detail.records));
     }
 }
